98c46e810f0f20cfadefdbcab7e14d716551927a
MXMK <mxmk1799@yandex.ru>
2019-10-23 19:50:36 +0300

    db

--- db/database.js
+++ db/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize('tourist_agency', 'root', 'password', {
-  host: 'localhost',
+module.exports = new Sequelize('heroku_2cb98397c9ba98f', 'b7ef66d3dc26f8', 'baf627fc', {
+  host: 'eu-cdbr-west-02.cleardb.net',
   dialect: 'mysql'
 });
+// mysql://b7ef66d3dc26f8:baf627fc@eu-cdbr-west-02.cleardb.net/heroku_2cb98397c9ba98f?reconnect=true
